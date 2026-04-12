3351aa8cfac5cc8874378bad9bd74cd4be5b36ff
sonyamg80 <jurioste4@gmail.com>
2026-04-11 21:18:37 -0600

    working on website

--- dist/NewearthquakeSite.380419b9.js
+++ dist/NewearthquakeSite.380419b9.js
@@ -720,9 +720,16 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 // test URL : https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/aka2026gsltwi.geojson
 const showMap = async function() {
     try {
-        const res = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/aka2026gsltwi.geojson");
+        const res = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson");
         const data = await res.json();
-        console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        console.log(data);
+        const quakeData = data.features.map((feature)=>({
+                id: feature.id,
+                location: feature.properties.place,
+                mag: feature.properties.mag
+            }));
+        console.log(quakeData);
     } catch (err) {
         alert(err);
     }
